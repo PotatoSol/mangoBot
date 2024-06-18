3f97206e4f3ec91d6eea75c398ca10f69fec4d70
PotatoSol <sol.mike.j@gmail.com>
2024-06-17 22:38:41 -0400

    Create fudge command, bot command, and helper function - TODO: Figure out how to pick up fudges from og mangb0t

--- scripts/custom/mangBotCommands.js
+++ scripts/custom/mangBotCommands.js
@@ -37,9 +37,35 @@
     $.inidb.set('duelwins', targetUser, 0);
     $.inidb.exists('duellosses', targetUser)
     $.inidb.set('duellosses', targetUser, 0)
-
-
     */
+
+
+    /* 
+    * Helper Functions
+    */
+    {
+        function fudge(senderUser, targetUser, reason){
+            //Check if they have fudge stacks
+            let currStacks = $.getIniDbNumber('fudgeStacks', targetUser);
+            let timestamp = $.getIniDbNumber('timestamp', targetUser);
+            let currTime = Math.floor(Date.now() / 1000);
+
+            if(timestamp + currStacks > currTime){ //still timed out
+                let remainingTime = timestamp + currStacks - currTime;
+                if(remainingTime <= 0){ //Should never happen
+                    remainingTime = 0;
+                }
+                $.inidb.set('fudgeStacks', targetUser, 600 + remainingTime);
+            }
+
+            let armorStacks = $.getIniDbNumber('armor', targetUser);
+            if(armorStacks >= 1){
+                $.inidb.incr('armor', targetUser, -1);
+                fudge(targetUser, senderUser, "The bullet reflected off " + senderUser + " \'s armor!");
+            }
+            $.timeoutUser(targetUser, $.getIniDbNumber('fudgeStacks', targetUser), "Fudged by " + senderUser + "! " + reason);
+        }
+    }
 
     $.bind('ircPrivateMessage', function (event) {
        
@@ -66,6 +92,53 @@
             args = event.getArgs(),
             tags = event.getTags(),
             action = args[0];
+
+        /*
+        * Fudge
+        */
+        if ($.equalsIgnoreCase(command, 'fudge')){ //Original Mangb0t will handle these
+            let currStacks = $.getIniDbNumber('fudgeStacks', targetUser);
+            let timestamp = $.getIniDbNumber('timestamp', targetUser);
+            let currTime = Math.floor(Date.now() / 1000);
+
+            if(timestamp + currStacks > currTime){ //still timed out
+                let remainingTime = timestamp + currStacks - currTime;
+                if(remainingTime <= 0){ //Should never happen
+                    remainingTime = 0;
+                }
+                $.inidb.set('fudgeStacks', targetUser, 600 + remainingTime);
+            }
+
+            let armorStacks = $.getIniDbNumber('armor', targetUser);
+            if(armorStacks >= 1){
+                $.inidb.incr('armor', targetUser, -1);
+                $.untimeoutUser(targetUser);
+                fudge(targetUser, senderUser, "The bullet reflected off " + senderUser + " \'s armor!");
+            }
+            
+        }
+
+        if ($.equalsIgnoreCase(command, 'botFudge')){
+            let targetUser = $.user.sanitize(args[1]);
+            let senderUser = $.user.sanitize(args[0]);
+            let reason = "";
+            if(args.length > 2){
+                reason += " \"" + args.splice(1).join(' '); + "\"";
+            }
+            fudge(targetUser, senderUser, reason);
+        }
+
+        /*
+        * Armor
+        */
+
+        if ($.equalsIgnoreCase(command, 'armor')){
+            
+        }
+
+        if ($.equalsIgnoreCase(command, 'botArmor')){
+
+        }
 
        
     });
